2c42b978b1c3925be184c180e88a3cf2689e5f64
aabenjamim <alanebenjamimdossantos@gmail.com>
2023-04-29 22:24:09 -0300

    feat: conta especial

--- src/contaBancaria/ContaEspecial.java
+++ src/contaBancaria/ContaEspecial.java
@@ -1,5 +1,31 @@
 package contaBancaria;
 
-public class ContaEspecial {
+public class ContaEspecial extends Conta {
+    
+    private double limite;
+
+    public ContaEspecial(int numero, String cpf){
+        super(numero, cpf);
+        this.limite = 1000;
+    }
+
+    public void usarLimite(double valor) {
+        double saldo = getSaldo();
+        double novoSaldo = saldo + valor;
+        double diferenca = Math.abs(novoSaldo);
+    
+        if (diferenca <= limite) {
+            if (novoSaldo < 0) {
+                debito(getSaldo());
+                limite -= saldo;
+            } else {
+                credito(novoSaldo);
+                limite -= valor;
+            }
+        } else {
+            System.out.println("Não é possível utilizar o limite disponível.");
+        }
+    }
     
 }
+
