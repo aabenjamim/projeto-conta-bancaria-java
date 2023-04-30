c1ffcdffe3a8a1f2915ce7b40857a91e1dab1044
aabenjamim <alanebenjamimdossantos@gmail.com>
2023-04-30 17:44:23 -0300

    feat: conta estudantil funcionando na tela inicial

--- src/contaBancaria/ContaEstudantil.java
+++ src/contaBancaria/ContaEstudantil.java
@@ -1,39 +1,124 @@
 package contaBancaria;
 
+import java.util.Scanner;
+
 public class ContaEstudantil extends Conta {
     private double limiteEstudantil;
+    int totalMovimentos = 0;
     
+    Scanner entrada = new Scanner(System.in);
+
     public ContaEstudantil(int numero, String cpf) {
         super(numero, cpf);
-        this.limiteEstudantil = 5000;
+        this.limiteEstudantil = 0;
     }
     
     public void usarEstudantil(double valor) {
-        if (limiteEstudantil - valor < 0) {
-            System.out.println("Não é possível realizar esta operação.");
+        if (limiteEstudantil + valor > 5000) {
+            System.out.println("Não é possível solicitar este valor de empréstimo.");
             return;
         }
+        if (totalMovimentos >= 10) {
+            System.out.println("Não é possível solicitar empréstimo após 10 movimentações.");
+            return;
+        }
+
+        limiteEstudantil += valor;
+        totalMovimentos++;
         super.credito(valor);
-        limiteEstudantil -= valor;
     }
     
     @Override
     public void debito(double valor) {
         double saldo = getSaldo();
-        if (saldo - valor < 0 || saldo - valor >= -limiteEstudantil) {
-            limiteEstudantil += saldo - valor;
-            super.debito(saldo);
-        } else if (saldo - valor < -limiteEstudantil) {
+        if (totalMovimentos >= 10) {
             System.out.println("Não é possível realizar esta operação.");
             return;
-        } else {
-            super.debito(valor);
         }
+        if (saldo < valor) {
+            System.out.println("SALDO INSUFICIENTE. PEDIR EMPRÉSTIMO ESTUDANTIL? S/N ");
+            String resp = entrada.next(); 
+            if(resp.equals("S")){
+                System.out.println("QUE VALOR VOCÊ QUER SOLICITAR? S/N ");
+                double emprestimo = entrada.nextDouble();
+                usarEstudantil(emprestimo);
+            } else{
+                System.out.println("DÉBITO NÃO EFETUADO");
+            }
+        }
+        super.debito(valor);
+        totalMovimentos++;
     }
 
     @Override
     public void credito(double valor) {
         super.credito(valor);
+        totalMovimentos++;
     }
+
+
+    public void visualizar(){    
+
+        System.out.println();
+        System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _ _ ");
+        System.out.println("BANCO AMARELO");
+        System.out.println("SEU DINHEIRO BEM CUIDADO");
+        System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _ _ ");
+        System.out.println("_ _ _ CONTA ESTUDANTIL _ _ _");
+        System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _ _ ");
+        System.out.println("SALDO ATUAL: R$ " + getSaldo());
+        System.out.println("MOVIMENTO - D-debito ou C-Crédito: _");
+        String mov = entrada.next();
+
+        if(mov.equals("D") && totalMovimentos<10){
+            System.out.println("Valor movimento: ");
+            Double valor = entrada.nextDouble();
+            debito(valor);
+            totalMovimentos++;
+            System.out.println("Novo saldo: " + getSaldo());
+        } else if(mov.equals("C") && totalMovimentos<10){
+            System.out.println("Valor movimento: ");
+            Double valor = entrada.nextDouble();
+            credito(valor);
+            totalMovimentos++;
+            System.out.println("Novo saldo: " + getSaldo());
+        } else{
+            System.out.println("Movimento inválido! ");
+        }
+
+        continuar();
+    }
+
+    public void continuar(){
+        System.out.println("DESEJA CONTINUAR? (S/N)");
+        String resposta = entrada.next();
+        if(resposta.equals("S")){
+            if(totalMovimentos<10){
+                System.out.println("DESEJA SOLICITAR EMPRÉSTIMO ESTUDANTIL? S/N ");
+                String resp = entrada.next(); 
+                if(resp.equals("S")){
+                    System.out.println("QUE VALOR VOCÊ QUER SOLICITAR EMPRESTADO? S/N ");
+                    double emprestimo = entrada.nextDouble();
+                    usarEstudantil(emprestimo);
+                } 
+                visualizar();
+            } else{
+                System.out.println("LIMITE DE MOVIMENTAÇÕES ATINGIDO.");
+                System.out.println("DESEJA SOLICITAR EMPRÉSTIMO ESTUDANTIL? S/N ");
+                String resp = entrada.next(); 
+                if(resp.equals("S")){
+                    System.out.println("QUE VALOR VOCÊ QUER SOLICITAR EMPRESTADO? S/N ");
+                    double emprestimo = entrada.nextDouble();
+                    usarEstudantil(emprestimo);
+                } else{
+                    System.out.println("OBRIGADA POR ESCOLHER NOSSOS SERVIÇOS!");
+                }
+            }
+        } else {
+            System.out.println("OBRIGADA POR USAR NOSSOS SERVIÇOS.");
+        }
+    }
+    
+
 }
 
--- src/contaBancaria/TelaInicial.java
+++ src/contaBancaria/TelaInicial.java
@@ -64,7 +64,12 @@ public class TelaInicial {
                 cem.visualizar();
                 break;
             case 5:
-                //
+                System.out.println("DIGITE O NÚMERO DA CONTA: ");
+                numero = entrada.nextInt();
+                System.out.println("DIGITE O NÚMERO DO CPF: ");
+                cpf = entrada.next();      
+                ContaEstudantil ces = new ContaEstudantil(numero, cpf);
+                ces.visualizar();
                 break;
             case 6:
                 System.out.println("OBRIGADA POR ESCOLHER NOSSO BANCO!");
@@ -78,71 +83,5 @@ public class TelaInicial {
 
     }
 
-/*int opcao;
-        String cpf;
-        int numero;
-        int diaAniversarioPoupanca;
-
-        System.out.println("BANCO AMARELO");
-        System.out.println("SEU DINHEIRO BEM CUIDADO");
-        System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _ _ ");
-        System.out.println("1 - CONTA POUPANÇA");
-        System.out.println("2 - CONTA CORRENTE");
-        System.out.println("3 - CONTA ESPECIAL");
-        System.out.println("4 - CONTA EMPRESA");
-        System.out.println("5 - CONTA ESTUDANTIL");
-        System.out.println("6 - SAIR");
-        System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _ _ ");
-        System.out.print("DIGITE O CODIGO DA OPÇÃO SELECIONADA: ");
-
-        opcao = entrada.nextInt();
-
-        if (opcao == 1) {
-            System.out.print("DIGITE O NÚMERO DA CONTA: ");
-            numero = entrada.nextInt();
-            entrada.nextLine();
-            System.out.print("DIGITE O CPF DO TITULAR: ");
-            cpf = entrada.nextLine();
-            System.out.print("DIGITE O DIA DE ANIVERSÁRIO DA CONTA: ");
-            diaAniversarioPoupanca = entrada.nextInt();
-            Conta conta = new ContaPoupanca(numero, cpf, diaAniversarioPoupanca);
-            System.out.println("CONTA CRIADA COM SUCESSO!");
-        } else if (opcao == 2) {
-            System.out.print("DIGITE O NÚMERO DA CONTA: ");
-            numero = entrada.nextInt();
-            entrada.nextLine();
-            System.out.print("DIGITE O CPF DO TITULAR: ");
-            cpf = entrada.nextLine();
-            Conta conta = new ContaCorrente(numero, cpf);
-            System.out.println("CONTA CRIADA COM SUCESSO!");
-        } else if (opcao == 3) {
-            System.out.print("DIGITE O NÚMERO DA CONTA: ");
-            numero = entrada.nextInt();
-            entrada.nextLine();
-            System.out.print("DIGITE O CPF DO TITULAR: ");
-            cpf = entrada.nextLine();
-            Conta conta = new ContaEspecial(numero, cpf);
-            System.out.println("CONTA CRIADA COM SUCESSO!");
-        } else if (opcao == 4) {
-            System.out.print("DIGITE O NÚMERO DA CONTA: ");
-            numero = entrada.nextInt();
-            entrada.nextLine();
-            System.out.print("DIGITE O CPF DO TITULAR: ");
-            cpf = entrada.nextLine();
-            Conta conta = new ContaEmpresa(numero, cpf);
-            System.out.println("CONTA CRIADA COM SUCESSO!");
-        } else if (opcao == 5){
-            System.out.print("DIGITE O NÚMERO DA CONTA: ");
-            numero = entrada.nextInt();
-            System.out.print("DIGITE O CPF DO TITULAR: ");
-            cpf = entrada.nextLine();
-            Conta conta = new ContaEstudantil(numero, cpf);
-            System.out.println("CONTA CRIADA COM SUCESSO!");
-        } else if(opcao == 6){
-            return;
-        } else{
-            System.out.println("Opção inválida!");
-        }
-
-    entrada.close();*/
+/*entrada.close();*/
 }
