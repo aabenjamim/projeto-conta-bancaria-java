008ea7027cdf467b042a98efdff1b398a02c932e
aabenjamim <alanebenjamimdossantos@gmail.com>
2023-04-29 22:43:49 -0300

    feat: conta empresa

--- src/contaBancaria/ContaEmpresa.java
+++ src/contaBancaria/ContaEmpresa.java
@@ -1,5 +1,46 @@
 package contaBancaria;
 
-public class ContaEmpresa {
+public class ContaEmpresa extends Conta {
+
+    private double emprestimoEmpresa;
+    private int totalMovimentos;
     
+    public ContaEmpresa(int numero, String cpf) {
+        super(numero, cpf);
+        this.emprestimoEmpresa = 0;
+    }
+    
+    public void pedirEmprestimo(double valor) {
+        double saldo = getSaldo();
+        if (emprestimoEmpresa + valor > 10000) {
+            System.out.println("Não é possível solicitar este valor de empréstimo.");
+            return;
+        }
+        if (totalMovimentos >= 10) {
+            System.out.println("Não é possível solicitar empréstimo após 10 movimentações.");
+            return;
+        }
+
+        emprestimoEmpresa += valor;
+        saldo += valor;
+    }
+    
+    @Override
+    public void debito(double valor) {
+        double saldo = getSaldo();
+        if (totalMovimentos >= 10 || saldo - valor < -emprestimoEmpresa) {
+            System.out.println("Não é possível realizar esta operação.");
+            return;
+        }
+        if (saldo < 0) {
+            emprestimoEmpresa += saldo;
+            debito(saldo);
+        }
+        debito(valor);
+    }
+
+    @Override
+    public void credito(double valor) {
+        credito(valor);
+    }
 }
